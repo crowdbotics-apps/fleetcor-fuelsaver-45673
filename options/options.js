79d16481031808a2d381aa6ce24127ee50eb9fe5
Crowdbotics <team@crowdbotics.com>
2024-01-29 18:41:26 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fleetcor-fuelsaver-45673",
+  url: "https://fleetcor-fuelsaver-45673.botics.co",
+  api: "https://fleetcor-fuelsaver-45673.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
